cac710f178850b0d42d4156311bb28d62058fd02
Bratie Fanut <bratiefanut@gmail.com>
2022-02-11 23:39:29 +0200

    Data Persistence 2
    - implementing Save/Load function to have persistent data between sessions

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
@@ -18,5 +19,36 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        LoadColor();
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color teamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.teamColor = teamColor;
+
+        string jsonData = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", jsonData);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string jsonData = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(jsonData);
+
+            teamColor = data.teamColor;
+        }
     }
 }
